31143e8283eb5aafe3dc328a7261f9d047a59c0a
Zer0o0 <henrique.cosmemk4@gmail.com>
2026-04-17 14:58:39 -0300

    UPDATE: cors

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ import cors from "cors";
 
 
 const app = express();
-app.use(cors())
+app.use(cors({
+    origin: 'https://get-in-ilp5.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.use(express.json());
 
